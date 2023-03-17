5f71bd85c5d66fa6f6bf6944b7b91c6bff414f7a
Maximo Mussini <maximomussini@gmail.com>
2023-03-17 13:15:26 -0300

    chore: defined test order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,8 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  config.order = :defined
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
