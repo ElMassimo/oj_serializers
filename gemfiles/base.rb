8543f2300401d140d2925d14a8f1f91d79f7d99c
Maximo Mussini <maximomussini@gmail.com>
2023-03-20 18:39:16 -0300

    chore: rubocop [skip ci]

--- gemfiles/base.rb
+++ gemfiles/base.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 def main_gemfile
-  "ENV['NO_RAILS'] = 'true'\n" + File.read('Gemfile').sub('gemspec', 'gemspec path: ".."')
+  "ENV['NO_RAILS'] = 'true'\n#{File.read('Gemfile').sub('gemspec', 'gemspec path: ".."')}"
 end
