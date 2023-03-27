e6b2e16f7cf6d2d10035175ccb8487a75f75fe82
Maximo Mussini <maximomussini@gmail.com>
2023-03-27 10:33:50 -0300

    chore: add alba and panko to memory usage benchmark

--- benchmarks/memory_usage_benchmark.rb
+++ benchmarks/memory_usage_benchmark.rb
@@ -12,6 +12,8 @@ RSpec.describe 'Memory Usage', :benchmark do
     AlbumSerializer.one_as_hash(album)
     LegacyAlbumSerializer.new(album).to_json
     AlbumBlueprint.render(album)
+    AlbumAlba.new(album).serialize
+    AlbumPanko.new.serialize_to_json(album)
   end
 
   def allocated_by(entry)
@@ -24,13 +26,15 @@ RSpec.describe 'Memory Usage', :benchmark do
       x.report('oj') { Oj.dump AlbumSerializer.one_as_json(album) }
       x.report('oj_hash') { Oj.dump AlbumSerializer.one_as_hash(album) }
       x.report('ams') { Oj.dump LegacyAlbumSerializer.new(album) }
+      x.report('alba') { AlbumAlba.new(album).serialize }
+      x.report('panko') { AlbumPanko.new.serialize_to_json(album) }
       x.report('blueprinter') { AlbumBlueprint.render(album) }
       x.compare!
     end
     entries = report.comparison.entries
     oj1, oj2, *rest = entries.map(&:label)
     expect([oj1, oj2]).to contain_exactly(*%w[oj_hash oj])
-    expect(rest).to eq %w[blueprinter ams]
+    expect(rest).to eq %w[panko blueprinter ams alba]
     expect(allocated_by(entries.first) / allocated_by(entries.last)).to be < 0.365
   end
 
@@ -40,11 +44,13 @@ RSpec.describe 'Memory Usage', :benchmark do
       x.report('oj') { Oj.dump AlbumSerializer.many_as_json(albums) }
       x.report('oj_hash') { Oj.dump AlbumSerializer.many_as_hash(albums) }
       x.report('ams') { Oj.dump(albums.map { |album| LegacyAlbumSerializer.new(album) }) }
+      x.report('alba') { AlbumAlba.new(albums).serialize }
+      x.report('panko') { Panko::ArraySerializer.new(albums, each_serializer: AlbumPanko).to_json }
       x.report('blueprinter') { AlbumBlueprint.render(albums) }
       x.compare!
     end
     entries = report.comparison.entries
-    expect(entries.map(&:label)).to eq %w[oj oj_hash blueprinter ams]
+    expect(entries.map(&:label)).to eq %w[oj oj_hash panko blueprinter ams alba]
     expect(allocated_by(entries.first) / allocated_by(entries.last)).to be < 0.33
   end
 end
